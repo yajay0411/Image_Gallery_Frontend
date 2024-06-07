15c1074df5571c161bd04a0aa7735f237312614a
Ajay Yadav <ajay.yadav@kriosispl.in>
2024-06-07 14:59:45 +0530

    css for image card

--- src/pages/InfiniteImageScroll/ImageCard.tsx
+++ src/pages/InfiniteImageScroll/ImageCard.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import css from "./inifiniteImageScroll.module.css";
 import { ArrowDownward } from "@material-ui/icons";
+import { Grid, IconButton } from "@material-ui/core";
 
 interface ImageDetails {
   likes?: number;
@@ -60,56 +61,85 @@ const ImageCard = ({ image }: { image: ImageDetails }) => {
           setImageHover(false);
         }}
       >
-        <img src={webformatURL} alt="title" className={css["img"]} />
-        {imageHover && (
-          <div className={css["hover-wrapper"]}>
-            <div>
-              <img
-                src={userImageURL}
-                alt="title"
-                className={css["image-uploader"]}
-              />
-              <span>
-                <strong>{user}</strong>
-              </span>
-            </div>
-            <div className="w-full flex justify-start ">
-              <span className="mr-5">
-                <strong>Likes:</strong> {likes}
-              </span>
-              <span className="mr-5">
-                <strong>Views:</strong> {views}
-              </span>
-              <span className="mr-5">
-                <strong>Downloads:</strong> {downloads}
-              </span>
-            </div>
-            {tags && tags.length > 0 && (
-              <div>
-                {tags.map((tag, index) => (
-                  <span
-                    key={index}
-                    className="inline-block bg-purple-200 rounded-sm px-3 py-1 text-[15px] font-semibold text-gray-700 mb-3 mx-2 shadow-md"
+        <div style={{ position: "relative" }}>
+          <Grid container direction="column">
+            <Grid item>
+              <img src={webformatURL} alt="title" className={css["img"]} />
+            </Grid>
+            {imageHover && (
+              <div className={css["download-btn-wrapper"]}>
+                <Grid item>
+                  <IconButton
+                    onClick={() => {
+                      if (largeImageURL) {
+                        downloadImage(largeImageURL);
+                      }
+                    }}
                   >
-                    <span>{`#${tag.trim()}`}</span>
-                  </span>
-                ))}
+                    <ArrowDownward />
+                  </IconButton>
+                </Grid>
               </div>
             )}
-          </div>
-        )}
-        {imageHover && (
-          <div
-            className="flex justify-end items-end absolute bottom-0 right-0 bg-white rounded-tl-full cursor-pointer"
-            onClick={() => {
-              if (largeImageURL) {
-                downloadImage(largeImageURL);
-              }
-            }}
-          >
-            <ArrowDownward />
-          </div>
-        )}
+            {imageHover && (
+              <Grid item>
+                <Grid
+                  container
+                  justifyContent="flex-start"
+                  alignItems="flex-end"
+                  spacing={2}
+                >
+                  <div className={css["hover-wrapper"]}>
+                    <Grid item>
+                      <img
+                        src={userImageURL}
+                        alt="title"
+                        className={css["image-uploader"]}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <span>
+                        <strong>{user}</strong>
+                      </span>
+                    </Grid>
+                    <Grid item>
+                      <Grid container spacing={1}>
+                        <Grid item>
+                          <span>
+                            <strong>Likes:</strong> {likes}
+                          </span>
+                        </Grid>
+                        <Grid item>
+                          <span>
+                            <strong>Views:</strong> {views}
+                          </span>
+                        </Grid>
+                        <Grid item>
+                          <span>
+                            <strong>Downloads:</strong> {downloads}
+                          </span>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      {tags && tags.length > 0 && (
+                        <Grid container spacing={2}>
+                          {tags.map((tag, index) => (
+                            <Grid item>
+                              <span key={index}>
+                                <span>{`#${tag.trim()}`}</span>
+                              </span>
+                            </Grid>
+                          ))}
+                        </Grid>
+                      )}
+                    </Grid>
+                  </div>
+                </Grid>
+              </Grid>
+            )}
+          </Grid>
+        </div>
       </div>
     </>
   );
