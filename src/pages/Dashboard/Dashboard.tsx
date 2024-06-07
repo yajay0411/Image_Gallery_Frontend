38eb61bf91103dcdcc96ba6c586a6382dfcb7afc
Ajay Yadav <ajay.yadav@kriosispl.in>
2024-06-07 12:34:27 +0530

    css update in dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,7 +1,17 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/Store";
+
 const Dashboard: React.FC = () => {
+  const mode = useSelector((state: RootState) => state.ThemeReducer.mode);
   return (
     <>
-      <div className="content">test</div>
+      <div
+        className={`content ${
+          mode === "dark" ? "content-dark" : "content-light"
+        } `}
+      >
+        test
+      </div>
     </>
   );
 };
